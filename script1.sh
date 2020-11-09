ec83714ba3bf05d61290c2e494abcadadc5ff8cb
frovator <41305274+frovator@users.noreply.github.com>
2020-11-09 14:42:16 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sh apt update -y 
+dpkg --configure -a
+apt-get -y update
 apt-get install apache2 -y 
 service apache2 start 
 echo '<html>Hello World from vm1</html>' >/var/www/html/index.html 
