cb5c1868137ceb8049d8ce5ab09b0ca290af0b2c
frovator <41305274+frovator@users.noreply.github.com>
2020-11-09 14:18:55 +0100

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sh sysctl net.ipv4.ip_forward=1 
+apt update -y 
+apt-get install apache2 -y 
+service apache2 start 
+echo '<html>Hello World from vm0</html>' >/var/www/html/index.html 
+service apache2 restart
