2a5450593ed39e9444ecf6f4802fb5cc31d9b123
frovator <41305274+frovator@users.noreply.github.com>
2020-11-09 14:42:46 +0100

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sh sysctl net.ipv4.ip_forward=1 
-apt update -y 
+sysctl net.ipv4.ip_forward=1 
+dpkg --configure -a
+apt-get update -y 
 apt-get install apache2 -y 
 service apache2 start 
 echo '<html>Hello World from vm0</html>' >/var/www/html/index.html 
